a9deac1c120af783a9cc92717b4c8d324c792ce8
Deepak Weljali <deepakweljali@gmail.com>
2020-01-21 15:10:25 +0530

    Added utility scripts

---
+++ infra.sh
@@ -0,0 +1,45 @@
+#Infra Deployment
+#!/bin/bash
+set -e
+echo "### Infra deployment for the provided sdlc environment"
+
+SDLC_ENVIRONMENT=$1
+BRANCH=$2
+
+
+if [[ "$#" -lt 2 ]]; then
+  echo "Exiting: Pipeline parameters are missing. Cannot execute the pipeline"
+  exit 1
+fi
+
+UTILITY_PATH='./datafoundry/scripts'
+
+cat <<EOF_code_params
+SDLC_ENVIRONMENT=$SDLC_ENVIRONMENT
+BRANCH=$BRANCH
+EOF_code_params
+
+# Check if we have the utility script and execute it
+UTILITY_SCRIPT="${UTILITY_PATH}/utility_script.sh"
+if [ ! -f "${UTILITY_SCRIPT}" ]; then
+  echo "FATAL: ${UTILITY_SCRIPT} not found. Unable to include shared functions."
+  exit 1
+fi
+. ${UTILITY_SCRIPT}
+
+#Deployment
+terraform init
+terraform plan
+terraform apply \
+  -var 'AWS_REGION=$AWS_REGION_NAME' 
+  -var 'VPC_CIDR=$VPC_CIDR' 
+  -var 'PUBLIC_SUBNET_CIDR=$PUBLIC_SUBNET_CIDR' 
+  -var 'EGRESS_PROTOCOL=$EGRESS_PROTOCOL' 
+  -var 'EGRESS_PORT=EGRESS_PORT' 
+  -var 'EGRESS_CIDR=$EGRESS_CIDR' 
+  -var 'INGRESS_PORT=$INGRESS_PORT' 
+  -var 'INGRESS_PROTOCOL=$INGRESS_PROTOCOL' 
+  -var 'INGRESS_CIDR=$INGRESS_CIDR' 
+  -var 'AMI=$AMI_ID' 
+  -var 'KEY_PATH=$KEY_PATH' 
+  -auto-approve
---
+++ utility_script.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# This should be included by each script to DRY things up.
+#
+SDLC_ENVIRONMENT=$1
+echo ' -- Utility functions and shared variables included.'
+###################
+# Global settings
+# Functions
+function fatal(){
+  echo "FATAL: $1"
+  exit 1
+}
+
+function set_environment(){
+  case "$SDLC_ENVIRONMENT" in
+    "stg"|"prod")
+      [ -z "$AWS_REGION" ] && AWS_REGION='us-east-1'
+      AWS_REGION_NAME='UsEast1'
+      ACCOUNT_NAME="data-foundry"
+      ACCOUNT_NUMBER="6542456899"
+      VPC_CIDR="10.0.0.0/16"
+      PUBLIC_SUBNET_CIDR="[10.0.1.0/28]"
+      EGRESS_PROTOCOL="-1"
+      EGRESS_PORT="0"
+      EGRESS_CIDR="[0.0.0.0/0]"
+      INGRESS_PORT="22"
+      INGRESS_PROTOCOL="tcp"
+      INGRESS_CIDR="[0.0.0.0/0]"
+      AMI_ID="ami-4fffc834"
+      KEY_PATH="mykey.pub"
+      ;;
+    *)
+      [ -z "$AWS_REGION" ] && AWS_REGION='us-east-1'
+      AWS_REGION_NAME='UsEast1'
+      ACCOUNT_NAME="data-foundry"
+      ACCOUNT_NUMBER="6542456899"
+      VPC_CIDR="10.0.0.0/16"
+      PUBLIC_SUBNET_CIDR="[10.0.1.0/28]"
+      EGRESS_PROTOCOL="-1"
+      EGRESS_PORT="0"
+      EGRESS_CIDR="[0.0.0.0/0]"
+      INGRESS_PORT="22"
+      INGRESS_PROTOCOL="tcp"
+      INGRESS_CIDR="[0.0.0.0/0]"
+      AMI_ID="ami-4fffc834"
+      KEY_PATH="mykey.pub"
+      ;;
+  esac
+}
+
+echo "Calling set environment"
+set_environment
+
+
+cat <<EOF_set_environment
+ Environment settings:
+      ACCOUNT_NAME=$ACCOUNT_NAME
+      ACCOUNT_NUMBER=$ACCOUNT_NUMBER
+      VPC_CIDR=$VPC_CIDR
+      PUBLIC_SUBNET_CIDR=$PUBLIC_SUBNET_CIDR
+      EGRESS_PROTOCOL=$EGRESS_PROTOCOL
+      EGRESS_PORT=$EGRESS_PORT
+      EGRESS_CIDR=$EGRESS_CIDR
+      INGRESS_PORT=$INGRESS_PORT
+      INGRESS_PROTOCOL=$INGRESS_PROTOCOL
+      INGRESS_CIDR=$INGRESS_CIDR
+      AMI_ID=$AMI_ID
+      KEY_PATH=$KEY_PATH
+EOF_set_environment
