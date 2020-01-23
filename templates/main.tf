7b2265cfc24178238524797166974cd04171df11
deepakweljali <deepakweljali@gmail.com>
2020-01-23 11:59:31 +0530

    Added code to delete infra

--- templates/main.tf
+++ templates/main.tf
@@ -13,30 +13,30 @@ module "rds"
   source = "../modules/rds"  
 }
 
-module "main-vpc" {
-  source     = "../modules/vpc"
-  ENV        = var.ENV
-  AWS_REGION = var.AWS_REGION
-  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
-  VPC_CIDR = var.VPC_CIDR
-}
+#module "main-vpc" {
+#  source     = "../modules/vpc"
+#  ENV        = var.ENV
+#  AWS_REGION = var.AWS_REGION
+#  PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
+#  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+#  VPC_CIDR = var.VPC_CIDR
+#}
 
-module "instances" {
-  source         = "../modules/instances"
-  ENV            = var.ENV
-  VPC_ID         = module.main-vpc.vpc_id
-  PUBLIC_SUBNETS = module.main-vpc.public_subnets
-  INGRESS_PORT = var.INGRESS_PORT
-  INGRESS_PROTOCOL = var.INGRESS_PROTOCOL
-  INGRESS_CIDR = var.INGRESS_CIDR
-  EGRESS_PORT = var.EGRESS_PORT
-  EGRESS_PROTOCOL = var.EGRESS_PROTOCOL
-  EGRESS_CIDR = var.EGRESS_CIDR
-  INSTANCE_TYPE = var.INSTANCE_TYPE
-  AMI_ID = var.AMI_ID
-  KEY_PATH = var.KEY_PATH
-}
+#module "instances" {
+#  source         = "../modules/instances"
+#  ENV            = var.ENV
+#  VPC_ID         = module.main-vpc.vpc_id
+#  PUBLIC_SUBNETS = module.main-vpc.public_subnets
+#  INGRESS_PORT = var.INGRESS_PORT
+#  INGRESS_PROTOCOL = var.INGRESS_PROTOCOL
+#  INGRESS_CIDR = var.INGRESS_CIDR
+#  EGRESS_PORT = var.EGRESS_PORT
+#  EGRESS_PROTOCOL = var.EGRESS_PROTOCOL
+#  EGRESS_CIDR = var.EGRESS_CIDR
+#  INSTANCE_TYPE = var.INSTANCE_TYPE
+#  AMI_ID = var.AMI_ID
+#  KEY_PATH = var.KEY_PATH
+#}
 
 output "RDS" {
   value = var.RDS
